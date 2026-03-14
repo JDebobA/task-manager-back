c98bf6f5d91918e3893afc9987003dec8359a9ca
JDebobA <deniszabskij430@gmail.com>
2026-03-14 20:48:03 +0700

    Err

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
+	imports: [
+		ConfigModule.forRoot({
 			isGlobal: true
 		}),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+		UserModule
+	]
 })
 export class AppModule {}
