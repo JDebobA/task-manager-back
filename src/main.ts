dd924c8198271bd8a515d0362462558cefa0ec0b
JDebobA <deniszabskij430@gmail.com>
2026-03-14 17:55:10 +0700

    Add swagger and modified Priority

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+
+	const configSwagger = new DocumentBuilder()
+		.setTitle('API')
+		.setDescription('Документация API')
+		.setVersion('1.0')
+		.build()
+
+	const document = SwaggerModule.createDocument(app, configSwagger)
+	SwaggerModule.setup('api-docs', app, document)
+
 	const config = app.get(ConfigService)
 	app.useGlobalPipes(
 		new ValidationPipe({
@@ -13,7 +24,6 @@ async function bootstrap() {
 		})
 	)
 
-	await app.listen(process.env.PORT ?? 3000)
 	await app.listen(config.getOrThrow<number>('PORT') ?? 3000)
 }
 bootstrap()
