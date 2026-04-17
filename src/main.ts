2beafc7c143eef45f50f1344281295784d5eec39
JDebobA <deniszabskij430@gmail.com>
2026-04-17 20:14:43 +0700

    Это Жопа

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	app.setGlobalPrefix('api')
+	app.enableCors({
+		origin: ['http://localhost:3000'],
+		credentials: true
+	})
 
 	const configSwagger = new DocumentBuilder()
 		.setTitle('API')
